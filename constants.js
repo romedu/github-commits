de53bd7726f89ce9396cc716e0020c078fb9c744
Roberto Meran <rmeran@contractor.appsbees.com>
2021-07-12 22:57:10 -0400

    fix: Fix an issue regarding fetch requests on Android devices

--- constants.js
+++ constants.js
@@ -1,1 +1,3 @@
-export const BASE_API_URL = "http://localhost:3000";
+import { Platform } from "react-native";
+
+export const BASE_API_URL = Platform.OS === "ios" ? "http://localhost:3000" : "http://10.0.2.2:3000";
